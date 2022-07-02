f449b66947e1b7f2de185351fc797a01d036ff2d
Dheeraj Borde <bordedheeraj.77@gmail.com>
2022-07-02 11:30:35 +0530

    ens shipped

--- deployEns.js
+++ deployEns.js
@@ -1,14 +1,15 @@
+const ethers = require('ethers');
+
 const express = require('express')
 const App = express();
 const path = require('path')
 const port = 5000
 App.use(express.static('public'))
-
+const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/7cedc93bca594509a5abbaae985320a6')
 // "web3": "^1.7.4",
 //     "web3-eth-ens": "^1.7.4"
 //     "@ensdomains/ensjs": "^3.0.0-alpha.2",
 
-//const ethers = require('ethers');
 // const {ethereum} = window
 // var Web3 = require("web3")
 // //var accounts = ethereum.enable();
@@ -32,14 +33,15 @@ App.get('/metadata', (req, res)=>{
     res.send(metadataNFT)
 })
 
-// App.get('/ens', async (req, res)=>{
-//     let query = req.query
-//     console.log(query)
-//     let address = await ens.getAddress(query.ens);
-    
-//     //let address = await ens.name(query.ens).getAddress();
-//     res.send(address)
-// })
+App.get('/ens', async (req, res)=>{
+    let query = req.query
+    console.log(query)
+ //   let address = await ens.getAddress(query.ens);
+    var address = await provider.resolveName(query.ens);
+    console.log(address)
+    //let address = await ens.name(query.ens).getAddress();
+    res.send(address)
+})
 
 App.listen(port, ()=>{
     console.log('Server started');
--- index.js
+++ index.js
@@ -1,20 +1,40 @@
 const express = require('express')
+const ethers = require('ethers');
 const App = express();
 const path = require('path')
 const port = 5000
 App.use(express.static('public'))
+let x = 0;
+const provider = new ethers.providers.JsonRpcProvider('https://mainnet.infura.io/v3/7cedc93bca594509a5abbaae985320a6')
 
-App.get('/metadata', (req, res)=>{
+App.get('/metadata', async (req, res)=>{
+    //console.log('req is defined',req)
+    let query = req.query
+    console.log('query is',query)
     let url = req.protocol + '://' + req.get('host') 
     console.log(url)
+    let metadataNFT = getNFTjson(url,query.x);
+    res.send(metadataNFT)
+})
+
+function getNFTjson (url,x){
+    
     let metadataNFT = { 
-        name: 'Mike',
+        name: `0x${x}`,
         description:'this is truts nft demo on testNet',
-        image: `${url}/frame-2.png`,
+        image: `${url}/${x}.png`,
         animation_url:`${url}/index.html`
     };
 
-    res.send(metadataNFT)
+    return metadataNFT;
+}
+
+App.get('/ens', async (req, res)=>{
+    let query = req.query
+    console.log(query)
+    var address = await provider.resolveName(query.ens);
+    console.log(address)
+    res.send(address)
 })
 
 App.listen(port, ()=>{
